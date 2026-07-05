715bb72408b39d5da97bf8f093cdaf4e5ee021d0
Ayub005 <G278005@globalbanking.ac.uk>
2026-07-05 15:13:12 +0100

    Add files via upload

---
+++ DDL/DML/insert_prescription.sql
@@ -0,0 +1,16 @@
+INSERT INTO `nhs_db`.`prescription`
+(`PrescriptionID`,
+`PatientID`,
+`DoctorID`,
+`MedicationID`,
+`DatePrescribed`,
+`Dosage`,
+`Instructions`)
+VALUES
+(<{PrescriptionID: }>,
+<{PatientID: }>,
+<{DoctorID: }>,
+<{MedicationID: }>,
+<{DatePrescribed: }>,
+<{Dosage: }>,
+<{Instructions: }>);
