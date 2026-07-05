a20270f7a42cbc8f68c04425755306eddaf1c0d3
Ayub005 <G278005@globalbanking.ac.uk>
2026-07-05 15:09:49 +0100

    Add files via upload

---
+++ DDL/DML/insert_appointment.sql
@@ -0,0 +1,16 @@
+INSERT INTO `nhs_db`.`appointment`
+(`AppointmentID`,
+`PatientID`,
+`DoctorID`,
+`ClinicID`,
+`AppointmentDate`,
+`AppointmentTime`,
+`Notes`)
+VALUES
+(<{AppointmentID: }>,
+<{PatientID: }>,
+<{DoctorID: }>,
+<{ClinicID: }>,
+<{AppointmentDate: }>,
+<{AppointmentTime: }>,
+<{Notes: }>);
